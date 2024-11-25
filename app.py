d25a770eb292e462460e1aba4b5223603449de2b
marslatt <marina_slatinska@yahoo.com>
2024-11-25 12:17:47 +0200

    FastAPI-PoC

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-# Import the libraries
-# from fastapi import FastAPI, Request
-# from transformers import pipeline
-# from transformers import AutoModelForCausalLM, AutoTokenizer
-# from pydantic import BaseModel
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from transformers import pipeline
@@ -45,27 +39,6 @@ async def chat(request: InputData):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
-#
-
-'''
-tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-model = AutoModelForCausalLM.from_pretrained("bert-base-cased")
-
-# ValueError: Cannot use chat template functions because tokenizer.chat_template is not set and no template argument 
-# was passed! For information about writing templates and setting the tokenizer.chat_template attribute, please see 
-# the documentation at https://huggingface.co/docs/transformers/main/en/chat_templating
-
-@app.post("/generate", response_model=OutputData)
-def generate(request: Request, input_data: InputData):
-    prompt = [{'role': 'user', 'content': input_data.prompt}]
-    inputs = tokenizer.apply_chat_template(prompt, add_generation_prompt=True, return_tensors='pt')
-    # prompt_length = inputs[0].shape[0]
-    # tokens = model.generate(inputs.to(model.device), max_new_tokens=1024, temperature=0.8, do_sample=True)
-    # response = tokenizer.decode(tokens[0][prompt_length:], skip_special_tokens=True)
-    response = tokenizer.decode(inputs[0])
-    return OutputData(response=response)
-'''
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
